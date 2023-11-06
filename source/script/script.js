fe33e7a6080c925e90e5bb042b2b9d6932014388
KXhLED <voit.kai@gmx.de>
2023-11-06 10:13:40 +0100

    dfas

--- source/script/script.js
+++ source/script/script.js
@@ -34,7 +34,7 @@ function toggleButtons(btnid) {
 
 function openLanguageSelector() {
   // Get the modal
-  var modal = document.getElementById("language");
+  var modal = document.getElementById("language-modal");
         
   // Get the button that opens the modal
   var btn = document.getElementById("language-button");
@@ -49,6 +49,39 @@ function openLanguageSelector() {
   
   // When the user clicks on <span> (x), close the modal
   span.onclick = function() {
+    closeLanguageSelector();
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
+
+function closeLanguageSelector() {
+  document.getElementById("language-modal").style.display = "none";
+}
+
+function openCartModal1() {
+  // Get the modal
+  var modal = document.getElementById("cart-modal");
+        
+  // Get the button that opens the modal
+  var btn = document.getElementById("cart-button");
+  
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+  
+  // When the user clicks the button, open the modal 
+  btn.onmouseenter = function() {
+    modal.style.display = "block";
+    modal.removeAttribute('autofocus');
+  }
+  
+  // When the user clicks on <span> (x), close the modal
+  btn.onclick = function() {
     modal.style.display = "none";
   }
   
@@ -58,4 +91,14 @@ function openLanguageSelector() {
       modal.style.display = "none";
     }
   }
+}
+
+function openCart() {
+  document.getElementById("mainSite").style.display = "none";
+  document.getElementById("cartSite").style.display = "block";
+}
+
+function openMain() {
+  document.getElementById("cartSite").style.display = "none";
+  document.getElementById("mainSite").style.display = "block";
 }
